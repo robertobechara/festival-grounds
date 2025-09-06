b75c010c62b99b9fdde02f014ce641463c49e7fb
Roberto Bechara <roberto.bechara@gmail.com>
2025-09-06 23:49:50 +0200

    add play/quit in main meny

--- Assets/_Project/Game/MainMenuUI.cs
+++ Assets/_Project/Game/MainMenuUI.cs
@@ -11,6 +11,14 @@ public class MainMenuUI : MonoBehaviour
 
     public void OnQuitButtonClicked()
     {
-        Application.Quit();
+        // This will run in a standalone build
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+
+            // This will run in the Unity Editor
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
